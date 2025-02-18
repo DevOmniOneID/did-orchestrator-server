ef42592f1e5ad1257c3033a32d4fe6863afd6e45
sjkim <sjkim@raoncorp.com>
2025-02-18 16:54:25 +0900

    권한이슈로 패브릭 스크립트 변경

--- shells/Fabric/start.sh
+++ shells/Fabric/start.sh
@@ -7,23 +7,27 @@ command="${PWD}/fabric-samples/bin/peer"
 eval $command
 
 if [ $? -eq 0 ]; then
-echo "installation checked"
+        echo "installation checked"
 else
-${PWD}/install-fabric.sh --fabric-version 2.5.11 binary
-${PWD}/install-fabric.sh --fabric-version 2.5.1 docker
-sleep 5
+        tar -xvf did-fabric-contract-1.0.0.tar.gz
+        tar -xvf fabric-samples-1.4.4.tar.gz
+        ${PWD}/install-fabric.sh --fabric-version 2.5.11 binary
+        ${PWD}/install-fabric.sh --fabric-version 2.5.1 docker
+        ${PWD}/fabric-samples/test-network/network.sh down
 fi
 
-${PWD}/fabric-samples/test-network/network.sh down
-sleep 5
-${PWD}/fabric-samples/test-network/network.sh up
-sleep 5
-${PWD}/fabric-samples/test-network/network.sh up -ca -s couchdb
-sleep 5
 ${PWD}/fabric-samples/test-network/network.sh createChannel -c $CHANNEL
-sleep 5
+${PWD}/fabric-samples/test-network/network.sh up -ca -s couchdb
+sleep 3
 
-${PWD}/fabric-samples/test-network/network.sh deployCC -c $CHANNEL -ccn $CHAINCODE_NAME -ccp ../../did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
+command="sh ${PWD}/status.sh" $CHANNEL $CHAINCODE_NAME
+eval $command
+
+if [ $? -eq 0 ]; then
+        echo "starting checked"
+else
+        ${PWD}/fabric-samples/test-network/network.sh deployCC -c $CHANNEL -ccn $CHAINCODE_NAME -ccp ${PWD}/did-fabric-contract/source/did-fabric-contract -ccl go -ccs 1
+fi
 
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/* ${PWD}/cert/sk
 cp ${PWD}/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/signcerts/* ${PWD}/cert/cert.pem
